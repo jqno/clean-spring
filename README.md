31394b12821f8d2477051f7148812f19c6a2f269
Jan Ouwens <jan.ouwens@gmail.com>
2021-06-18 14:21:21 +0200

    Applies formatting

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ Sample application taken from a [Spring tutorial](https://spring.io/guides/gs/re
 Run as follows:
 * `mvn clean spring-boot:run`
 * Open `http://localhost:8080/greeting`
+
+Formatter:
+* Run with `mvn spotless:apply` to apply formatting (or from your IDE)
+* Run with `mvn install` or `mvn verify` to check formatting (it will fail if it's incorrect)
+* There's also an IntelliJ plugin for Prettier, but I haven't tried it.
+
+We can configure it to apply formatting on commit using a commit hook.
--- src/main/java/com/jumbo/demo/controller/GreetingController.java
+++ src/main/java/com/jumbo/demo/controller/GreetingController.java
@@ -1,9 +1,7 @@
 package com.jumbo.demo.controller;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 import com.jumbo.demo.model.Greeting;
-
+import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,11 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-	private static final String template = "Hello, %s!";
-	private final AtomicLong counter = new AtomicLong();
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
 
-	@GetMapping("/greeting")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
-	}
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
 }
